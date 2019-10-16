82018cb751d322057ac82bb20244b38d8ee57469
Edwardo Ramirez Datu <Edward_R_Datu@manulife.com>
2019-10-16 10:11:55 +0800

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage.component'
 
+const HatsPage = (props) => {
+    console.log(props);
+    return (
+    <div>
+        <h1>Hats Page</h1>
+    </div>
+)
+}
+
+const HatsPageDetails = (props) => {
+    console.log(props);
+    return (
+    <div>
+        <h1>Hats Page Details</h1>
+    </div>
+)
+}
+
 function App() {
     return (
         <div>
-            <HomePage></HomePage>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route exact path='/shop/hats' component={HatsPage}/>
+                <Route exact path='/shop/hats/:hatID' component={HatsPageDetails}/>
+            </Switch>
         </div>
     );
 }
